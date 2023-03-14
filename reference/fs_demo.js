f83d987f41d5466fe41befd10e2a0261015405ae
Viiuska <87257685+Viiuska@users.noreply.github.com>
2023-03-14 17:23:03 +0200

    simple url and os usage

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -1,13 +1,14 @@
+/*
 const fs = require('fs')
 const path = require('path')
 
-/*
+
 //new folder
 fs.mkdir(path.join(__dirname, '/test'), {}, err =>{
     if(err) throw err;
     console.log("Folder created...")
 })
-*/
+
 
 //Create and write to file
 fs.writeFile(path.join(__dirname, '/test', "hello.txt"),
@@ -24,3 +25,18 @@ fs.writeFile(path.join(__dirname, '/test', "hello.txt"),
     console.log("File written to...")
 })
 })
+
+
+//Read file
+fs.readFile(path.join(__dirname, '/test', 'hello.txt'),'utf8', (err, data) =>{
+    if(err) throw err;
+    console.log(data)
+})
+
+//Rename file
+fs.rename(path.join(__dirname, '/test', 'hello.txt'),
+    path.join(__dirname, '/test', 'welcome.txt'), err =>{
+    if(err) throw err;
+    console.log("File renamed...")
+})
+*/
