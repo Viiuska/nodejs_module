3dc10f461bd87513a18d25e4b5b39d59115b5636
Viiuska <87257685+Viiuska@users.noreply.github.com>
2023-03-14 16:32:42 +0200

    Simple class created

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+/*
 const person = require('./person');
-
 console.log(person)
+*/
+const Person = require('./person');
+
+const person1 = new Person("Pedro Pascal", 47);
+
+person1.greeting();
--- person.js
+++ person.js
@@ -1,6 +1,19 @@
+/*
 const person = {
     name:'Pedro Pascal',
     age:'47'
 }
+*/
 
-module.exports = person;
+class Person {
+    constructor(name, age){
+        this.name=name;
+        this.age=age;
+    }
+
+    greeting(){
+        console.log("My name is "+ this.name +" and I am "+this.age)
+    }
+}
+
+module.exports = Person;
